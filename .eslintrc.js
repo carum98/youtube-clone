11799abbc83752664d6fcb39e3cc7ea598d27f9b
Carlos Umaña Acevedo <carumtito@gmail.com>
2021-12-11 17:37:29 -0600

    Setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'plugin:vue/base',
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript/recommended',
+    'prettier',
+  ],
+  rules: {},
+  env: {},
+}
