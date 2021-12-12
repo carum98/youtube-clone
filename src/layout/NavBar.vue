21f01e7f7c6ed9eb19a45bbad6716f909249ac0a
Carlos Umaña Acevedo <carumtito@gmail.com>
2021-12-12 00:24:09 -0600

    NavBar

--- src/layout/NavBar.vue
+++ src/layout/NavBar.vue
@@ -1,5 +1,25 @@
 <script setup lang="ts"></script>
 
 <template>
-    <header id="navbar"></header>
+    <header id="navbar">
+        <section>
+            <i class="material-icons-outlined">menu</i>
+            <img class="logo" src="../assets/yt-logo-dark.svg" />
+        </section>
+        <section class="search">
+            <input type="text" placeholder="Search" />
+            <button class="search__button search__button--search">
+                <i class="material-icons-outlined">search</i>
+            </button>
+            <button class="search__button search__button--mic">
+                <i class="material-icons-outlined">mic</i>
+            </button>
+        </section>
+        <section>
+            <i class="material-icons-outlined">videocam</i>
+            <i class="material-icons-outlined">apps</i>
+            <i class="material-icons-outlined">notifications</i>
+            <i class="material-icons">account_circle</i>
+        </section>
+    </header>
 </template>
